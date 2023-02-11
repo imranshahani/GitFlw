08692a3e8e2af3e9d6ce9f20ff04dc2c48a40166
imranshahani <100560548+imranshahani@users.noreply.github.com>
2023-02-11 12:00:20 +0500

    Test (#2) (#5)
    * change file (#1)
    
    * Addition by Joel
    
    ---------
    
    Co-authored-by: Joel Chris <cjoel5287@gmail.com>
    Co-authored-by: Joel Christopher  Bhatti <123950725+joelbhatti@users.noreply.github.com>

--- readme.js
+++ readme.js
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-=======
+
 dwsnsdlet x = 5;
-<<<<<<< HEAD
-<<<<<<< HEAD
 const a = 25;
 let b = 65;
 c = a + b;
 console.log(c);
-=======
->>>>>>> Features
-=======
+
 console.logThis is the merging test command
->>>>>>> Features
+
 This is Test Commit
->>>>>>> d820e2bdefb1815c3c4857a751e0a5cf6e9ed282
